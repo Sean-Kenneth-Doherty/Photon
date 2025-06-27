a452a38c7e47dadcf0a06984fc84921d7763a067
Your <you@example.com>
2025-06-28 08:40:18 +1200

    "Feat-Keyboard-Navigation-Culling"

--- photon_app.py
+++ photon_app.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow, QDockWidget, QTextEdit, QWidget, QVBoxLayout, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QDockWidget, QTextEdit, QWidget, QVBoxLayout, QMessageBox, QMenu
+from PyQt6.QtGui import QAction, QKeySequence
 from PyQt6.QtCore import Qt
 from photon.views.folder_tree_view import FolderTreeView
 from photon.views.thumbnail_grid_view import ThumbnailGridView
@@ -28,10 +29,108 @@ class PhotonApp(QMainWindow):
 
         self.create_dock_widgets()
         self.apply_dark_theme()
+        self._create_actions()
+        self._create_menu_bar()
 
         # Example: Load a catalog (this can be triggered by user action later)
         # For now, we'll keep it commented out to avoid blocking during initial setup
         # self.load_catalog(r"C:\Pictures\Lightroom\Catalogs\2025\2025\2025.lrcat")
+
+    def _create_actions(self):
+        self.next_photo_action = QAction("Next Photo", self)
+        self.next_photo_action.setShortcut(QKeySequence(Qt.Key.Key_Right))
+        self.next_photo_action.triggered.connect(self._next_photo)
+        self.addAction(self.next_photo_action)
+
+        self.prev_photo_action = QAction("Previous Photo", self)
+        self.prev_photo_action.setShortcut(QKeySequence(Qt.Key.Key_Left))
+        self.prev_photo_action.triggered.connect(self._prev_photo)
+        self.addAction(self.prev_photo_action)
+
+        self.pick_photo_action = QAction("Pick Photo", self)
+        self.pick_photo_action.setShortcut(QKeySequence(Qt.Key.Key_P))
+        self.pick_photo_action.triggered.connect(self._pick_photo)
+        self.addAction(self.pick_photo_action)
+
+        self.reject_photo_action = QAction("Reject Photo", self)
+        self.reject_photo_action.setShortcut(QKeySequence(Qt.Key.Key_X))
+        self.reject_photo_action.triggered.connect(self._reject_photo)
+        self.addAction(self.reject_photo_action)
+
+        self.rate_1_action = QAction("Rate 1 Star", self)
+        self.rate_1_action.setShortcut(QKeySequence(Qt.Key.Key_1))
+        self.rate_1_action.triggered.connect(lambda: self._rate_photo(1))
+        self.addAction(self.rate_1_action)
+
+        self.rate_2_action = QAction("Rate 2 Stars", self)
+        self.rate_2_action.setShortcut(QKeySequence(Qt.Key.Key_2))
+        self.rate_2_action.triggered.connect(lambda: self._rate_photo(2))
+        self.addAction(self.rate_2_action)
+
+        self.rate_3_action = QAction("Rate 3 Stars", self)
+        self.rate_3_action.setShortcut(QKeySequence(Qt.Key.Key_3))
+        self.rate_3_action.triggered.connect(lambda: self._rate_photo(3))
+        self.addAction(self.rate_3_action)
+
+        self.rate_4_action = QAction("Rate 4 Stars", self)
+        self.rate_4_action.setShortcut(QKeySequence(Qt.Key.Key_4))
+        self.rate_4_action.triggered.connect(lambda: self._rate_photo(4))
+        self.addAction(self.rate_4_action)
+
+        self.rate_5_action = QAction("Rate 5 Stars", self)
+        self.rate_5_action.setShortcut(QKeySequence(Qt.Key.Key_5))
+        self.rate_5_action.triggered.connect(lambda: self._rate_photo(5))
+        self.addAction(self.rate_5_action)
+
+        self.rate_0_action = QAction("Rate 0 Stars", self)
+        self.rate_0_action.setShortcut(QKeySequence(Qt.Key.Key_0))
+        self.rate_0_action.triggered.connect(lambda: self._rate_photo(0))
+        self.addAction(self.rate_0_action)
+
+    def _create_menu_bar(self):
+        menu_bar = self.menuBar()
+
+        file_menu = menu_bar.addMenu("&File")
+        # Add actions to file menu later (e.g., Open Catalog)
+
+        view_menu = menu_bar.addMenu("&View")
+        view_menu.addAction(self.folder_tree_view.toggleViewAction())
+        view_menu.addAction(self.thumbnail_grid_view.toggleViewAction())
+        view_menu.addAction(self.photo_preview_view.toggleViewAction())
+
+        culling_menu = menu_bar.addMenu("&Culling")
+        culling_menu.addAction(self.next_photo_action)
+        culling_menu.addAction(self.prev_photo_action)
+        culling_menu.addSeparator()
+        culling_menu.addAction(self.pick_photo_action)
+        culling_menu.addAction(self.reject_photo_action)
+        culling_menu.addSeparator()
+        culling_menu.addAction(self.rate_1_action)
+        culling_menu.addAction(self.rate_2_action)
+        culling_menu.addAction(self.rate_3_action)
+        culling_menu.addAction(self.rate_4_action)
+        culling_menu.addAction(self.rate_5_action)
+        culling_menu.addAction(self.rate_0_action)
+
+    def _next_photo(self):
+        print("Next photo")
+        # TODO: Implement logic to select the next photo in the thumbnail grid
+
+    def _prev_photo(self):
+        print("Previous photo")
+        # TODO: Implement logic to select the previous photo in the thumbnail grid
+
+    def _pick_photo(self):
+        print("Pick photo")
+        # TODO: Implement logic to mark the current photo as picked
+
+    def _reject_photo(self):
+        print("Reject photo")
+        # TODO: Implement logic to mark the current photo as rejected
+
+    def _rate_photo(self, rating: int):
+        print(f"Rate photo: {rating} stars")
+        # TODO: Implement logic to set the rating of the current photo
 
     def load_catalog(self, catalog_path: str):
         if not os.path.exists(catalog_path):
